f912114d0fdc45e6ad0e9ed15f924a4c7322d6bb
Nhân Lê <nhan.le1407bkhcm@hcmut.edu.vn>
2023-03-07 21:10:14 +0700

    first commit

---
+++ consumer.py
@@ -0,0 +1,27 @@
+from confluent_kafka import Consumer
+
+def read_ccloud_config(config_file):
+    conf = {}
+    with open(config_file) as fh:
+        for line in fh:
+            line = line.strip()
+            if len(line) != 0 and line[0] != "#":
+                parameter, value = line.strip().split('=', 1)
+                conf[parameter] = value.strip()
+    return conf
+
+props = read_ccloud_config("client.properties")
+props["group.id"] = "python-group-1"
+props["auto.offset.reset"] = "earliest"
+
+consumer = Consumer(props)
+consumer.subscribe(["topic_2"])
+try:
+    while True:
+        msg = consumer.poll(1.0)
+        if msg is not None and msg.error() is None:
+            print("key = {key} value = {value}".format(key=msg.key().decode('utf-8'), value=msg.value().decode('utf-8')))
+except KeyboardInterrupt:
+    pass
+finally:
+         consumer.close()
